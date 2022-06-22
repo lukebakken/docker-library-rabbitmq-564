a7e952b6ac76b4bfa732ec48cf192ddf656f2ca7
Luke Bakken <luke@bakken.io>
2022-06-22 07:14:54 -0700

    Update README

--- init.sh
+++ init.sh
@@ -13,3 +13,8 @@ git submodule update --init
 make -C ./tls-gen/basic CN=localhost
 
 docker build --tag docker-library-564:latest .
+
+docker run --rm --publish 5672:5672 \
+    --publish 5671:5671 \
+    --publish 15672:15672 \
+    --publish 15671:15671 docker-library-564:latest
